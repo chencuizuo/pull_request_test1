ff7904d98606b01f713f8ce4080fa81f8814c8cd
Satanzcc <55731905+Satanzcc@users.noreply.github.com>
2023-08-18 23:32:00 +0800

    Update print.c
    添加了打印整金字塔

--- print.c
+++ print.c
@@ -15,3 +15,26 @@ int main()
   }	
   return 0;	
 }
+
+//打印整金字塔
+int main()	
+{	
+  int i,space,rows,k=0;	
+  printf("Enter the number of rows: ");	
+  scanf("%d",&rows);	
+  for(i=1;i<=rows;++i)	
+  {	
+    for(space=1;space<=rows-i;++space)	
+    {	
+       printf("  ");	
+    }	
+    while(k!=2*i-1)	
+    {	
+       printf("* ");	
+       ++k;	
+    }	
+    k=0;	
+    printf("\n");	
+  }	
+  return 0;	
+}
